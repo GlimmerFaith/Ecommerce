9aca5397ab391909f79c41afaa514ca7dcc0f145
XiaoYang <XY@git.com>
2023-06-28 16:35:41 +0800

    second commit
    Signed-off-by: XiaoYang <XY@git.com>

---
+++ store/src/main/java/com/xy/storeApplication.java
@@ -0,0 +1,13 @@
+package com.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class storeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(storeApplication.class, args);
+    }
+
+}
